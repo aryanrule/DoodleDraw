4ec1aa2c81aeee12b1ad181e31cfcfa3de3efb13
Aryan Sharma <aryansharma99107@gmail.com>
2025-08-18 00:07:44 +0530

    mongodb establlish

---
+++ src/app/connect_db/mongo.ts
@@ -0,0 +1,30 @@
+"use server"
+import mongoose, { Mongoose } from "mongoose"
+
+const db_url = process.env.MONGO_URL!;
+
+interface MongoConnection {
+    connection : Mongoose | null
+    promise : Promise<Mongoose> | null;
+}
+let cached: MongoConnection = (global as any).mongoose;
+if (!cached) {
+    cached = (global as any).mongoose = { connection: null, promise: null };
+}
+const connect_DB =  async () => {
+    if(cached.connection){
+        return cached.connection;
+    }
+    if(!db_url){
+        console.log("no dbUrl is here");
+    }
+
+    try{  
+        cached.connection = await mongoose.connect(db_url , {bufferCommands: false}) 
+    }catch(error){
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
+}
+
+export default connect_DB;
