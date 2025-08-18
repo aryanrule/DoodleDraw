2c487e77c203f6c77ed6ff2a489ac4e8913e1fa2
Aryan Sharma <aryansharma99107@gmail.com>
2025-08-18 23:14:38 +0530

    landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,76 @@
 import Image from "next/image";
-
+import sideimg from '../../public/sideimg.png'
+import Link from "next/link";
+import { ArrowLeftIcon, GitHubLogoIcon, TwitterLogoIcon } from "@radix-ui/react-icons";
+import Navbar from "./_components/Navbar";
+import Hero from "./_components/Hero";
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  <div className="bg-gray-900">
+      <Navbar />
+      <Hero />
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+      <div className="w-full flex justify-center dark:bg-gray-900 bg-gradient-to-b dark:from-gray-900 dark:via-gray-900 from-[#D1D1D1] via-[#D1D1D1] to-white py-4">
+        <div className=" w-[1300px] flex sm:flex-row flex-col gap-6">
+          <div className="bg-[#171717] sm:w-[70%] w-[100%] flex justify-center items-center rounded-xl border-black border-[1px]">
+            <img
+              className="w-[480px]"
+              src="https://assets-global.website-files.com/62d58a323cbc396f06a780aa/652724d77e8a69256e0fdaa4_ai%20diagram%202.gif"
+              alt="gif"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="bg-gray-100 dark:bg-gray-700 sm:h-full  sm:w-[30%] w-[100%] rounded-xl overflow-hidden flex justify-center items-center">
+            <Image
+              src={sideimg}
+              alt="sidebar image"
+              className=" w-auto h-[350px] rounded-lg "
+            />
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      </div>
+
+      <div className="w-full flex justify-center  bg-white dark:bg-gray-900 py-4">
+        <div className=" w-[1300px] flex sm:flex-row flex-col gap-6 ">
+          <div className="sm:w-[40%] w-full  flex flex-col gap-6 ">
+            <div className="sm:h-[50%] w-full bg-gray-100 dark:bg-slate-700 rounded-2xl flex items-center sm:p-12 p-10 text-[19px] font-[500] cursor-pointer ">
+              "Perfect your diagrams with precision using our versatile feature,
+              making editing a breeze."
+            </div>
+            <div className="sm:h-[50%] w-full bg-gray-100 dark:bg-slate-700 rounded-2xl flex items-center sm:p-12 p-10 text-[19px] font-[500] cursor-pointer ">
+              "Craft professional-grade diagrams effortlessly with our intuitive
+              eraser tool"
+            </div>
+          </div>
+          <div className="sm:w-[60%] w-full h-50 bg-gray-100 dark:bg-slate-700 rounded-2xl flex justify-center items-center sm:p-14 p-10 sm:text-[26px] text-[19px]  cursor-pointer font-[500]">
+            "Streamline your diagramming process with our intuitive eraser
+            feature, allowing you to focus on your ideas without worry."
+          </div>
+        </div>
+      </div>
+      {/* Pricing */}
+      <div className="w-full flex justify-center py-20 bg-white dark:bg-gray-900 ">
+        <div className=" w-[1300px] ">
+          <h1 className="flex w-full justify-center text-[40px] font-bold">
+            Pricing
+          </h1>
+          {/* <Pricing /> */}
+        </div>
+      </div>
+      {/* footer */}
+      <footer className="bg-white dark:bg-gray-900 flex justify-center py-5">
+        <div className="flex justify-between w-[1300px]">
+          <p className="mt-8 text-[14px] text-gray-900 dark:text-gray-300 sm:mt-0">
+            &copy; 2022. build from scratch by{" "}
+            <Link className="font-[600]" href={"https://github.com/yadavvshall"}>
+              Vishal
+            </Link>
+            .
+          </p>
+          <div className="flex gap-4">
+            <Link href={'https://x.com/BsB02266322?t=56DmZEf-ILT0VReOtf1t7g&s=09'} target="_blank" ><TwitterLogoIcon className="size-6 "/></Link>
+            <Link href={'https://github.com/bhaskar-005'} target="_blank"><GitHubLogoIcon className="size-6 "/></Link>
+          </div>
+        </div>
       </footer>
     </div>
   );
